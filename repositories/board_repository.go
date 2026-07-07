a7464d7da8eb00f69eb04f577b0d9161944805a7
Ciptaaaa <diazekacipta467@gmail.com>
2026-07-07 18:35:22 +0700

    feat:add table DB board and migrations, Create Table Board, boardRepo, ETC

---
+++ repositories/board_repository.go
@@ -0,0 +1,22 @@
+package repositories
+
+import (
+	"github.com/Ciptaaaa/Project-Management.git/config"
+	"github.com/Ciptaaaa/Project-Management.git/models"
+)
+
+type BoardRepository interface {
+Create(board *models.Board)error
+}
+
+type boardRepository struct {
+}
+
+func NewBoardRepository() BoardRepository {
+	return &boardRepository{}
+}
+
+func (r *boardRepository) Create(board *models.Board)error{
+	return config.DB.Create(board).Error
+}
+
---
+++ services/board_service.go
@@ -0,0 +1,33 @@
+package services
+
+import (
+	"errors"
+
+	"github.com/Ciptaaaa/Project-Management.git/models"
+	"github.com/Ciptaaaa/Project-Management.git/repositories"
+	"github.com/google/uuid"
+)
+
+type BoardService interface {
+
+}
+
+type boardService struct {
+	boardRepo repositories.BoardRepository
+	userRepo repositories.UserRepository
+}
+
+func newBoardService(boardRepo repositories.BoardRepository,
+	userRepo repositories.UserRepository,) BoardService {
+	return &boardService{boardRepo,userRepo }
+}
+
+func (s *boardService) Create (board *models.Board) error {
+	user, err:= s.userRepo.FindByPublicID(board.OwnerPublicID.String())
+	if err != nil{
+		return errors.New("owner not found")
+	}
+	board.PublicID = uuid.New()
+	board.OwnerID = user.InternalID
+	return s.boardRepo.Create(board)
+}
